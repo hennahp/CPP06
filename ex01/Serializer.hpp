9d58d8622e47710ab59f304e89d24936d377cf16
Henna Parveen <hennaparveen77@gmail.com>
2026-01-09 10:11:46 +0400

    started ex01

---
+++ ex01/Serializer.hpp
@@ -0,0 +1,27 @@
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+
+#include <cstdint>
+
+class Serializer
+{
+    private:
+        Serializer();
+        ~Serializer();
+        Serializer(const Serializer &other);
+        Serializer &operator=(const Serializer &other);
+
+    public:
+        static uintptr_t serialize(Data* ptr);
+        static Data* deserialize(uintptr_t raw);
+};
+
+#endif
