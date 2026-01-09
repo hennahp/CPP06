4a7c36164cb64437910b632c6104aa278bdac397
hennahp <hennaparveen77@gmail.com>
2026-01-09 06:29:49 +0400

    need to do double and float

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -31,42 +31,68 @@ static bool isIntLiteral(const std::string &str)
     return (*endptr == '\0');
 }
 
-static bool isFloatLiteral(const std::string &str)
-{
-    if(str[str.length() - 1] != 'f')
-        return false;
+// static bool isFloatLiteral(const std::string &str)
+// {
+//     if(str[str.length() - 1] != 'f')
+//         return false;
     
-    char *endptr;
-    std::strtof(str.c_str(), &endptr);
-    return (*endptr == 'f' && *(endptr + 1) == '\0');
-}
+//     char *endptr;
+//     std::strtof(str.c_str(), &endptr);
+//     return (*endptr == 'f' && *(endptr + 1) == '\0');
+// }
 
-static bool isDoubleLiteral(const std::string &str)
-{
-    char *endptr;
-    std::strtod(str.c_str(), &endptr);
-    return (*endptr == '\0');
-}
+// static bool isDoubleLiteral(const std::string &str)
+// {
+//     char *endptr;
+//     std::strtod(str.c_str(), &endptr);
+//     return (*endptr == '\0');
+// }
 
 static void printPseudoLiterals(const std::string &literal)
 {
-   std::cout << "char: impossible" << std::endl;
-   std::cout << "int: impossible" << std::endl;
+   std::cout << "char   :impossible" << std::endl;
+   std::cout << "int    :impossible" << std::endl;
 
    if(literal == "nan" || literal == "+inf" || literal == "-inf")
    {
-    std::cout << "float: " << literal << "f" << std::endl;
-    std::cout << "double: " << literal << std::endl;
+    std::cout << "float  :" << literal << "f" << std::endl;
+    std::cout << "double :" << literal << std::endl;
    }
    else
    {
-    std::cout << "float: " << literal << std::endl;
-    std::cout << "double: " << literal.substr(0, literal.length() - 1) << std::endl;
+    std::cout << "float  :" << literal << std::endl;
+    std::cout << "double :" << literal.substr(0, literal.length() - 1) << std::endl;
    }
 }
 
 void ScalarConverter::convert(const std::string &literal)
 {
+    if(isCharLiteral(literal))
+    {
+        char c = literal[0];
+        std::cout << "char   :'" << c << "'" << std::endl;
+        std::cout << "int    :" << static_cast<int>(c) << std::endl;
+        std::cout << "float  :" << static_cast<float>(c) << "f" << std::endl;
+        std::cout << "double :" << static_cast<double>(c) << std::endl;
+        return;
+    }
+    if(isIntLiteral(literal))
+    {
+        long value = std::strtol(literal.c_str(), NULL, 10);
+        if(value < 0 || value > 127)
+            std::cout << "char   :impossible" << std::endl;
+        else if(!std::isprint(static_cast<char>(value)))
+            std::cout << "char   :Non displayable" << std::endl;
+        else
+            std::cout << "char   :'" << static_cast<char>(value) << "'" << std::endl;
+        if(value < std::numeric_limits<int>::min() || value > std::numeric_limits<int>::max())
+            std::cout << "int    :impossible" << std::endl;
+        else
+            std::cout << "int    :" << static_cast<int>(value) << std::endl;
+        std::cout << "float  :" << static_cast<float>(value) << ".0f" << std::endl;
+        std::cout << "double :" << static_cast<double>(value) << ".0" << std::endl;
+        return;
+    }
     if(isPseudoLiteral(literal))
     {
         printPseudoLiterals(literal);
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -4,6 +4,14 @@
 #include <string>
 #include <iostream>
 
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define RESET   "\033[0m"
+
 class ScalarConverter
 {
     private:
