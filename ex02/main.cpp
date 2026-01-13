98ecd18fd5c18b3c00ac337774d075c8e53b77a6
Henna Parveen <hennaparveen77@gmail.com>
2026-01-13 10:48:26 +0400

    done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -9,11 +9,17 @@ int main()
     std::srand(static_cast<unsigned int>(std::time(NULL)));
 
     Base* ptr = generate();
+    Base *john = 
+    Base* invalid = NULL;
 
     std::cout << GREEN <<"Identifying using pointer:" << RESET << std::endl;
     identify(ptr);
     std::cout << GREEN <<"Identifying using reference:" << RESET << std::endl;
     identify(*ptr);
+    std::cout << GREEN <<"Identifying nullptr using pointer:" << RESET << std::endl;
+    identify(invalid);
+    std::cout << GREEN <<"Identifying nullptr using reference:" << RESET << std::endl;
+    identify(*invalid);
     delete ptr;
 
     return 0;
